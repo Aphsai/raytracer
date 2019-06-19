62b95bebbb1e2b37460d3d0f5f5743cce7502586
Saksham Aggarwal <saksham.a@hotmail.ca>
2019-06-19 14:47:39 -0400

    added anti-aliasing

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <limits>
 #include <fstream>
+#include <stdlib.h>
+#include <time.h>
 #include "vec3.h"
 #include "ray.h"
 #include "primitives.h"
-#include <limits>
+#include "camera.h"
 
 
 //TODO: Move to primitives header file
@@ -23,26 +26,31 @@ int main() {
 	std::ofstream file;
 	const int ROWS = 100;
 	const int COLUMNS = 200;
-
-	vec3 origin (0.0, 0.0, 0.0);
-	vec3 vertical (0.0, 2.0, 0.0);
-	vec3 horizontal (4.0, 0.0, 0.0);
-	vec3 ll_corner (-2.0, -1.0, -1.0);
+	const int SAMPLES = 100;
 
 	Collideable* list[2];
 	list[0] = new Sphere(vec3(0, 0, -1), 0.5);
 	list[1] = new Sphere(vec3(0, -100.5, -1), 100);
 	Collideable* world = new CollideableList(list, 2);
+
+	Camera camera;
+	srand(time(NULL));
 	
 	file.open("ray_test.ppm");
 	file << "P3\n" << COLUMNS << " " << ROWS << "\n255\n";
 	for (int row = ROWS - 1; row >= 0; row--) {
 		for (int col = 0; col < COLUMNS; col++) {
-			ray r (origin, ll_corner + float(col) / float(COLUMNS) * horizontal + float(row) / float(ROWS) * vertical);
-			vec3 v = color(r, world);
-			int ir = int(v.x() * 255.99);
-			int ig = int(v.y() * 255.99);
-			int ib = int(v.z() * 255.99);
+			vec3 averaged_color (0, 0, 0);
+			for (int s = 0; s < SAMPLES; s++) {
+				float u = float(col + (float)(rand()) / RAND_MAX) / float(COLUMNS);
+				float v = float(row + (float)(rand()) / RAND_MAX) / float(ROWS);
+				ray r = camera.get_ray(u, v);
+				averaged_color += color(r, world);
+			}
+			averaged_color /= float(SAMPLES);
+			int ir = int(averaged_color.x() * 255.99);
+			int ig = int(averaged_color.y() * 255.99);
+			int ib = int(averaged_color.z() * 255.99);
 			file << ir << " " << ig << " " << ib << "\n";
 		}
 	}
