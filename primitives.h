fb9d7f5aaf5acddd39c2820fb22e9ff4c4bae53c
Aphsai <saksham.a@hotmail.ca>
2019-06-20 15:33:10 -0400

    resetting

--- primitives.h
+++ primitives.h
@@ -1,66 +1,25 @@
 #pragma once
-#include "collideable.h"
+#include "ray.h"
 
 class Material;
 
-class CollideableList : public Collideable {
-	public:
-		CollideableList() {}
-		CollideableList(Collideable **l, int n) { list = l; list_size = n; }
-		virtual bool hit(const ray& r, float t_min, float t_max, HitRecord& rec) const;
-		Collideable **list;
-		int list_size;
+class Entity {
+        virtual bool hit(const ray& r) {}
 };
 
-bool CollideableList::hit(const ray& r, float t_min, float t_max, HitRecord& rec) const {
-	HitRecord temp_rec;
-	bool hit_anything = false;
-	double closest_so_far = t_max;
-	for (int i = 0; i < list_size; i++) {
-		if (list[i]->hit(r, t_min, closest_so_far, temp_rec)) {
-			hit_anything = true;
-			closest_so_far = temp_rec.t;
-			rec = temp_rec;
-		}
-	}
-	return hit_anything;
-}
 
-class Sphere: public Collideable {
+class Sphere : public Entity {
 	public:
 		Sphere() {}
+		Sphere(vec3 c, float r) { center = c; radius = r; }
 		Sphere(vec3 c, float r, Material* m) { center = c; radius = r; material = m; }
-		virtual bool hit(const ray& r, float t_min, float t_max, HitRecord& rec) const;
+        
+        bool hit(const ray& r);
 		float radius;
 		Material *material;
 		vec3 center;
 };
 
-bool Sphere::hit(const ray& r, float t_min, float t_max, HitRecord& rec) const {
-	vec3 oc = r.origin() - center;
-	float a = dot(r.direction(), r.direction());
-	float b = 2.0 * dot (oc, r.direction());
-	float c = dot (oc, oc) - radius * radius;
-
-	float discriminant = (b * b - 4 * a * c);
-	if (discriminant > 0) {
-		float discriminant_root = sqrt(discriminant);
-		float time_till_hit = (-b - discriminant_root) / (2 * a);
-		if (time_till_hit < t_max && time_till_hit > t_min) {
-			rec.t = time_till_hit;
-			rec.p = r.point_at_parameter(time_till_hit);
-			rec.normal = (rec.p - center) / radius;
-			rec.mat_ptr = material;
-			return true;
-		}
-		time_till_hit = (-b + discriminant_root) / (2 * a);
-		if (time_till_hit > t_max && time_till_hit < t_min) {
-			rec.t = time_till_hit;
-			rec.p = r.point_at_parameter(time_till_hit);
-			rec.normal = (rec.p - center) / radius;
-			rec.mat_ptr = material;
-			return true;
-		}
-	}
-	return false;
+bool Sphere::hit(const ray& r) {
+    
 }
--- raytracer.cpp
+++ raytracer.cpp
@@ -3,53 +3,27 @@
 #include <limits>
 
 #include "primitives.h"
-#include "camera.h"
-#include "utility.h"
-#include "material.h"
 
-
-vec3 color(const ray& r, Collideable *world, int depth) {
-	HitRecord rec;
-	if (world->hit(r, 0.001, std::numeric_limits<float>::max(), rec)) {
-		ray scattered;
-		vec3 attenuation;
-		if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
-			return attenuation * color (scattered, world, depth + 1);
-		}
-		return vec3(0, 0, 0);
-	}
-
-	vec3 unit_direction = unit_vector(r.direction());
-	float t = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
-}
+vec3 color(ray &r, Entity* shape) {}
 
 int main() {
 	std::ofstream file;
-	const int ROWS = 100;
-	const int COLUMNS = 200;
+	const int HEIGHT = 720;
+	const int WIDTH = 1280;
 	const int SAMPLES = 100;
 	srand(time(NULL));
-	Collideable* list[4];
-	list[0] = new Sphere(vec3(0, 0, -1), 0.5, new Lambertian(vec3(0.8, 0.3, 0.3)));
-	list[1] = new Sphere(vec3(0, -100.5, -1), 100, new Lambertian(vec3(0.8, 0.8, 0.0)));
-	list[2] = new Sphere(vec3(-1, 0, -1), 0.5, new Dielectric(1.5));
-	list[3] = new Sphere(vec3(1, 0, -1), 0.5, new Metal(vec3(0.8, 0.8, 0.8), 0.3));
-	Collideable* world = new CollideableList(list, 4);
-	Camera camera;
-	
+
 	file.open("ray_test.ppm");
 	std::cout << "Writing to file..." << std::endl;
-	file << "P3\n" << COLUMNS << " " << ROWS << "\n255\n";
-	std::cout << "Generating colors..." << std::endl;
-	for (int row = ROWS - 1; row >= 0; row--) {
-		for (int col = 0; col < COLUMNS; col++) {
+	file << "P3\n" << WIDTH << " " << HEIGHT << "\n255\n";
+	std::cout << "Generating pixels..." << std::endl;
+    Entity* sphere = new Sphere( vec3(0, 0, 0), 0.5 );
+	for (int row = HEIGHT - 1; row >= 0; row--) {
+		for (int col = 0; col < WIDTH; col++) {
 			vec3 averaged_color (0, 0, 0);
 			for (int s = 0; s < SAMPLES; s++) {
-				float u = float(col + get_random_float()) / float(COLUMNS);
-				float v = float(row + get_random_float()) / float(ROWS);
-				ray r = camera.get_ray(u, v);
-				averaged_color += color(r, world, 0);
+                ray r;
+				averaged_color += color(r, sphere);
 			}
 			averaged_color /= float(SAMPLES);
 			averaged_color = vec3( sqrt(averaged_color.x()), sqrt(averaged_color.y()) ,sqrt(averaged_color.z()));
